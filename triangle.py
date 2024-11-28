ac4873f728772cc3941ea0de34b9530a6468a29e
Dmitrii Abramov <dmitriiabramov@MacBook-Air-Dmitrii.local>
2024-11-28 11:03:40 +0300

    er

--- triangle.py
+++ triangle.py
@@ -23,7 +23,7 @@ class TriangleTestCase(unittest.TestCase):
         self.assertEqual(res, 29)
     
     def test_triangle_perimeter_2(self):
-        res = perimeter(-45, 55, 90)
+        res = perimeter(-45, -55, 90)
         self.assertEqual(res, 190)
 
 
